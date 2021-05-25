5fdf779c588ce51232bae28674cfc2c41df80394
黄锦涛 <7792275+shutiaojun@user.noreply.gitee.com>
2021-05-25 17:32:22 +0800

    二次提交

---
+++ src/common/constConfig.js
@@ -0,0 +1,9 @@
+
+const baseConst={
+    // SYSTEMAPI:'http://localhost:5000',
+    SYSTEMAPI:'http://120.24.182.42:777/',
+    BASEINFOAPI:'http://localhost:8001',
+    VERSION:'0.1.2.0',
+}
+
+export default baseConst;
